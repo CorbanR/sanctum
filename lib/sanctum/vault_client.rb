54677e41f8e7ae047e42f7c6b5440738bbdfe093
Corban Raun <craun@instructure.com>
2018-05-15 15:20:07 -0600

    Remove superfluous `raise`
    Make vault_client more of a factory method

--- lib/sanctum/vault_client.rb
+++ lib/sanctum/vault_client.rb
@@ -2,33 +2,29 @@ require 'vault'
 
 module Sanctum
   class VaultClient
-    attr_reader :vault_client
 
-    def initialize(vault_address, vault_token)
-      @vault_client = Vault::Client.new(address: vault_address, token: vault_token)
-      check_token
+    def self.build(vault_address, vault_token)
+      vault_client = Vault::Client.new(address: vault_address, token: vault_token)
+      check_token(vault_client)
+      vault_client
     end
 
-    def check_token
+    def self.check_token(vault_client)
       response = vault_client.request(:get, "v1/auth/token/lookup-self")
       renewable = response[:data][:renewable]
 
       if renewable
         creation_ttl = response[:data][:creation_ttl]
         remaining = response[:data][:ttl]
-        thirty_percent = (creation_ttl * 0.30).to_i
+        fifty_percent = (creation_ttl * 0.50).to_i
 
-        renew_token(creation_ttl) if remaining < thirty_percent
+        renew_token(vault_client, creation_ttl) if remaining < fifty_percent
       end
-    rescue => e
-      raise e
     end
 
-    def renew_token(increment)
+    def self.renew_token(vault_client, increment)
       payload = {"increment": increment}.to_json
       vault_client.request(:post, "v1/auth/token/renew-self", payload)
-    rescue => e
-      raise e
     end
 
   end
