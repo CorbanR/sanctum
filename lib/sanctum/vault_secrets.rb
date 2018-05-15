ce5f89fb0dcb34f9c1ee45bfc09d83dbc2eb090c
Corban Raun <craun@instructure.com>
2018-05-15 15:22:12 -0600

    Change class methods to instance methods
    change class variable to instance variable.

--- lib/sanctum/vault_secrets.rb
+++ lib/sanctum/vault_secrets.rb
@@ -1,38 +1,44 @@
 module Sanctum
   class VaultSecrets
     include Colorizer
+    attr_reader :vault_client, :prefix
 
-    def self.get(vault_client, prefix)
-      if invalid_prefix?(vault_client, prefix)
+    def initialize(vault_client, prefix)
+      @vault_client = vault_client
+      @prefix = prefix
+    end
+
+    def get
+      if invalid_prefix?
         raise yellow("Warning: Vault prefix: '#{prefix}' does not exist.. ")
       end
 
-      @secrets_from_vault = Hash.new
-      @secrets_from_vault[prefix] = JSON(list_recursive(vault_client, prefix).to_json)
-      @secrets_from_vault
+      secrets_from_vault = Hash.new
+      secrets_from_vault[prefix] = JSON(list_recursive(prefix).to_json)
+      secrets_from_vault
     end
 
-    def self.invalid_prefix?(vault_client, prefix)
+    def invalid_prefix?
       vault_client.logical.list(prefix).empty?
     end
 
-    def self.list_recursive(vault, path, parent = '')
-      me = File.join(parent, path)
-      result = vault.logical.list(me).inject({}) do |hash, item|
+    def list_recursive(prefix, parent = '')
+      me = File.join(parent, prefix)
+      result = vault_client.logical.list(me).inject({}) do |hash, item|
         case item
         when /.*\/$/
-          hash[item.gsub(/\/$/, '').to_sym] = list_recursive(vault, item, me)
+          hash[item.gsub(/\/$/, '').to_sym] = list_recursive(item, me)
         else
-          hash[item.to_sym] = read_data(vault, item, me)
+          hash[item.to_sym] = read_data(item, me)
         end
         hash
       end
       result
     end
 
-    def self.read_data(vault, item, parent = '')
+    def read_data(item, parent = '')
       me = File.join(parent, item)
-      vault.logical.read(me).data
+      vault_client.logical.read(me).data
     end
 
   end
